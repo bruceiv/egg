7e778c93bb7994e49f3d504f92176cee528b1195
Aaron Moss <a3moss@uwaterloo.ca>
2014-02-04 13:56:44 -0500

    Incorporate flags library (from typegraph) and tests

---
+++ utils/flags.hpp
@@ -0,0 +1,175 @@
+#pragma once
+
+/*
+ * Copyright (c) 2013 Aaron Moss
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <cstdint>
+
+namespace flags {
+
+/// Gets the array element containing an index
+static inline uint64_t el(uint64_t i) { return i >> 6; }
+
+/// Gets the bit index in an array element of an index
+static inline uint64_t shft(uint64_t i) { return 63 - (i & ((1 << 6) - 1)); }
+
+/// Gets the mask of an index inside an element
+static inline uint64_t mask(uint64_t i) { return UINT64_C(1) << shft(i); }
+
+/// Gets the index of the leftmost bit in a byte (counting from the left)
+static const char lt[256] = {
+	-1, 7, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 4,
+	 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+	 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+	 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+/// Gets the number of bits set in a byte
+static const char ct[256] = {
+	0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,  // 15
+	1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  // 31
+	1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,  // 63
+	1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,  // 127
+	1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,  
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+	4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8   // 255
+};
+
+/// Gets the index of the high bit inside an element (0 is MSB, 63 is LSB)
+static uint64_t high(uint64_t e) {
+	uint64_t t;
+	if      ( e == 0 )        return -1;
+	else if ( (t = e >> 56) ) return      lt[t];
+	else if ( (t = e >> 48) ) return  8 + lt[t];
+	else if ( (t = e >> 40) ) return 16 + lt[t];
+	else if ( (t = e >> 32) ) return 24 + lt[t];
+	else if ( (t = e >> 24) ) return 32 + lt[t];
+	else if ( (t = e >> 16) ) return 40 + lt[t];
+	else if ( (t = e >>  8) ) return 48 + lt[t];
+	else                      return 56 + lt[e];
+}
+
+static inline uint64_t count(uint64_t e) {
+	return ct[e & 0xff]
+	     + ct[(e >>  8) & 0xff]
+	     + ct[(e >> 16) & 0xff]
+	     + ct[(e >> 24) & 0xff]
+	     + ct[(e >> 32) & 0xff]
+	     + ct[(e >> 40) & 0xff]
+	     + ct[(e >> 48) & 0xff]
+	     + ct[e >> 56];
+}
+
+/// zero all flags
+inline void clear(uint64_t* a, uint64_t n) { for (uint64_t i = 0; i < n; ++i) a[i] = UINT64_C(0); }
+
+/// true if the i'th bit of a is set
+inline bool get(const uint64_t* a, uint64_t i) { return (a[el(i)] & mask(i)) != 0; }
+
+/// sets the i'th bit of a
+inline void set(uint64_t* a, uint64_t i) { a[el(i)] |= mask(i); }
+
+/// unsets the i'th bit of a
+inline void unset(uint64_t* a, uint64_t i) { a[el(i)] &= ~mask(i); }
+
+/// checks if the bitflags are zeroed
+inline bool is_zero(const uint64_t* a, uint64_t n) {
+	for (uint64_t i = 0; i < n; ++i) if ( a[i] != 0 ) return false;
+	return true;
+}
+
+/// takes the union of two sets of bitflags a and b, writing it into c
+inline void set_union(const uint64_t* a, const uint64_t* b, uint64_t* c, uint64_t n) {
+	for (uint64_t i = 0; i < n; ++i) c[i] = a[i] | b[i];
+}
+
+/// takes the intersection of two sets of bitflags a and b, writing it into c
+inline void set_intersection(const uint64_t* a, const uint64_t* b, uint64_t* c, uint64_t n) {
+	for (uint64_t i = 0; i < n; ++i) c[i] = a[i] & b[i];
+}
+
+/// takes the set difference of two sets of bitflags a and b, writing it into c
+inline void set_difference(const uint64_t* a, const uint64_t* b, uint64_t* c, uint64_t n) {
+	for (uint64_t i = 0; i < n; ++i) c[i] = a[i] & ~b[i];
+}
+
+/// gets the number of set bits before i of a
+uint64_t rank(const uint64_t* a, uint64_t i) {
+	uint64_t c = 0;
+	//check previous elements
+	for (uint64_t j = 0; j < el(i); ++j) {
+		c += count(a[j]);
+	}
+	//mask off high bits of this element and count
+	c += count(a[el(i)] & ~((mask(i) << 1)-1));
+	return c;
+}
+
+/// gets the index of the first bit of a (length n) set
+uint64_t first(const uint64_t* a, uint64_t n) {
+	for (uint64_t i = 0; i < n; ++i) if ( a[i] != 0 ) return i*64 + high(a[i]);
+	return -1;
+}
+
+/// gets the index of the next bit after i of a (length n) set
+uint64_t next(const uint64_t* a, uint64_t n, uint64_t i) {
+	uint64_t j = el(i);
+	// mask off lower bits of this element
+	uint64_t t = a[j] & (mask(i)-1);
+	if ( t != 0 ) return j*64 + high(t);
+	// if nothing there, check later elements
+	for (++j; j < n; ++j) if ( a[j] != 0 ) return j*64 + high(a[j]);
+	return -1;
+}
+
+/// gets the count of set bits of a (length n)
+uint64_t count(const uint64_t* a, uint64_t n) {
+	uint64_t c = 0;
+	for (uint64_t i = 0; i < n; ++i) {
+		c += count(a[i]);
+	}
+	return c;
+}
+	
+} /* namespace flags */
+
---
+++ utils/tests/tests.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2013 Aaron Moss
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/** Tests for utility code. Returns 0 for all passed, 1 otherwise */
+
+#include <cstdint>
+
+#include "test.hpp"
+
+#include "../flags.hpp"
+
+void test_flags(tester& test) {
+	test.setup("flags");
+
+	uint64_t a[2];
+
+	// Test that these are zeroed as expected
+	flags::clear(a, 2);
+	test.equal(a[0], 0); test.equal(a[1], 0);
+
+	// Test the basic element, bit-index, and mask functions
+	test.equal(flags::el(0),     UINT64_C(0), "el(0)");
+	test.equal(flags::shft(0),   UINT64_C(63), "shft(0)");
+	test.equal(flags::mask(0),   UINT64_C(0x8000000000000000), "mask(0)");
+	test.equal(flags::el(42),    UINT64_C(0), "el(42)");
+	test.equal(flags::shft(42),  UINT64_C(21), "shft(42)");
+	test.equal(flags::mask(42),  UINT64_C(0x0000000000200000), "mask(42)");
+	test.equal(flags::el(63),    UINT64_C(0), "el(63)");
+	test.equal(flags::shft(63),  UINT64_C(0), "shft(63)");
+	test.equal(flags::mask(63),  UINT64_C(0x0000000000000001), "mask(63)");
+	test.equal(flags::el(64),    UINT64_C(1), "el(64)");
+	test.equal(flags::shft(64),  UINT64_C(63), "shft(64)");
+	test.equal(flags::mask(64),  UINT64_C(0x8000000000000000), "mask(64)");
+	test.equal(flags::el(71),    UINT64_C(1), "el(71)");
+	test.equal(flags::shft(71),  UINT64_C(56), "shft(71)");
+	test.equal(flags::mask(71),  UINT64_C(0x0100000000000000), "mask(71)");
+	test.equal(flags::el(127),   UINT64_C(1), "el(127)");
+	test.equal(flags::shft(127), UINT64_C(0), "shft(127)");
+	test.equal(flags::mask(127), UINT64_C(0x0000000000000001), "mask(127)");
+
+	// Test that set and get work together as expected
+	flags::set(a, 0);   test.check(flags::get(a, 0),   "set then get 0");
+	flags::set(a, 42);  test.check(flags::get(a, 42),  "set then get 42");
+	flags::set(a, 63);  test.check(flags::get(a, 63),  "set then get 63");
+	test.equal(a[0], UINT64_C(0x8000000000200001), "a[0] value post set 1");
+	test.equal(a[1], UINT64_C(0x0000000000000000), "a[1] value post set 1");
+	
+	flags::set(a, 64);  test.check(flags::get(a, 64),  "set then get 64");
+	flags::set(a, 71);  test.check(flags::get(a, 71),  "set then get 71");
+	flags::set(a, 127); test.check(flags::get(a, 127), "set then get 127");
+	test.equal(a[0], UINT64_C(0x8000000000200001), "a[0] value post set 2");
+	test.equal(a[1], UINT64_C(0x8100000000000001), "a[1] value post set 2");
+
+	// Test that unsetting bits works as well
+	flags::unset(a, 0);   test.check(!flags::get(a, 0),   "unset then get 0");
+	flags::unset(a, 42);  test.check(!flags::get(a, 42),  "unset then get 42");
+	flags::unset(a, 63);  test.check(!flags::get(a, 63),  "unset then get 63");
+	flags::unset(a, 64);  test.check(!flags::get(a, 64),  "unset then get 64");
+	flags::unset(a, 71);  test.check(!flags::get(a, 71),  "unset then get 71");
+	flags::unset(a, 127); test.check(!flags::get(a, 127), "unset then get 127");
+
+	// Test that the bitflags look like I expect
+	test.equal(a[0], UINT64_C(0x0000000000000000), "a[0] value post unset");
+	test.equal(a[1], UINT64_C(0x0000000000000000), "a[1] value post unset");
+	
+	uint64_t b[6] = {
+		UINT64_C(0xfedcba9876543210), UINT64_C(0x0edcba9876543210),
+		UINT64_C(0x000cba9876543210), UINT64_C(0x0000000000043210),
+		UINT64_C(0x0000000000000010), UINT64_C(0x0000000000000000)
+	};
+	a[0] = UINT64_C(0x0000000000000000); a[1] = b[2];
+
+	// Test high bit function
+	test.equal(flags::high(b[0]),  0, "high(b[0])");
+	test.equal(flags::high(b[1]),  4, "high(b[1])");
+	test.equal(flags::high(b[2]), 12, "high(b[2])");
+	test.equal(flags::high(b[3]), 45, "high(b[3])");
+	test.equal(flags::high(b[4]), 59, "high(b[4])");
+	test.equal(flags::high(b[5]), -1, "high(b[5])");
+
+	// Test count bits
+	test.equal(flags::count(b[0]), 32, "count(b[0])");
+	test.equal(flags::count(b[1]), 28, "count(b[1])");
+	test.equal(flags::count(b[2]), 22, "count(b[2])");
+	test.equal(flags::count(b[3]),  5, "count(b[3])");
+	test.equal(flags::count(b[4]),  1, "count(b[4])");
+	test.equal(flags::count(b[5]),  0, "count(b[5])");
+
+	test.equal(flags::count(b, 6), 88, "count(b)");
+	test.equal(flags::count(a, 2), 22, "count(a)");
+
+	// Test rank
+	test.equal(flags::rank(b,   0),  0, "rank(b, 0)");
+	test.equal(flags::rank(b,   1),  1, "rank(b, 1)");
+	test.equal(flags::rank(b,   8),  7, "rank(b, 8)");
+	test.equal(flags::rank(b,  63), 32, "rank(b, 63)");
+	test.equal(flags::rank(b,  64), 32, "rank(b, 64)");
+	test.equal(flags::rank(b,  72), 35, "rank(b, 72)");
+	test.equal(flags::rank(b, 127), 60, "rank(b, 127)");
+
+	// Test first and next
+	test.equal(flags::first(b, 6),    0, "first(b)");
+	test.equal(flags::first(b+1, 5),  4, "first(b+1)");
+	test.equal(flags::first(b+5, 1), -1, "first(b+5)");
+	test.equal(flags::first(a, 2),   76, "first(a)");
+
+	test.equal(flags::next(b, 6,  0),    1, "next(b, 0)");
+	test.equal(flags::next(b, 6, 68),   69, "next(b, 68)");
+	test.equal(flags::next(b+4, 2, 60), -1, "next(b+4, 60)");
+	test.equal(flags::next(a, 2, 32),   76, "next(a, 32)");
+	
+	test.cleanup();
+}
+
+int main(int argc, char** argv) {
+	tester test;
+
+	test_flags(test);
+	
+	return test.success() ? 0 : 1;
+}
+
